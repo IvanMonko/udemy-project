8fe4d63620183c7ad28262d69a3cce3ce8d02893
Ivan <ivanmonko61@gmail.com>
2020-06-21 18:14:56 +0200

    second commit

--- js/команды для юзера.js
+++ js/команды для юзера.js
@@ -14,4 +14,5 @@ answers[0] = prompt('what is your name?', '');
 answers[1] = prompt('what is your surname?', '');
 answers[2] = prompt('how old are you?', '');
 
-document.write(answers);
+console.log(typeof(answers));
+console.log(typeof(null));
